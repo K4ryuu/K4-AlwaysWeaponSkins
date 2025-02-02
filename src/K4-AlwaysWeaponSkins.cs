58583dfa8ecd22ee29e41641c04ef6ecf5f08441
K4ryuu <104531589+K4ryuu@users.noreply.github.com>
2025-02-02 22:03:59 +0100

    feat: Refactor team handling to use signature hook and update module version to 1.1.0

--- src/K4-AlwaysWeaponSkins.cs
+++ src/K4-AlwaysWeaponSkins.cs
@@ -1,5 +1,4 @@
 using System.Collections.ObjectModel;
-using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes;
 using CounterStrikeSharp.API.Modules.Memory;
@@ -13,16 +12,16 @@ namespace K4AlwaysWeaponSkins
 	{
 		public override string ModuleName => "CS2 Always Weapon Skins";
 		public override string ModuleAuthor => "K4ryuu @ KitsuneLab";
-		public override string ModuleDescription => "Apply inventory skins to opposing teams aswell.";
-		public override string ModuleVersion => "1.0.1";
+		public override string ModuleDescription => "Apply inventory skins to opposing teams as well.";
+		public override string ModuleVersion => "1.1.0";
 
-		public Dictionary<CCSPlayerController, CsTeam> HandledPlayers = [];
-		public Dictionary<CCSPlayerController, List<string>> TeamWeapons = [];
+		public MemoryFunctionVoid<int> GetTeamNumber { get; } = new(GameData.GetSignature("GetTeamNumber"));
+		public Dictionary<CCSPlayerController, Queue<string>> PlayerSkins { get; } = [];
 
-		public static readonly ReadOnlyDictionary<string, CsTeam> TeamSkins = new ReadOnlyDictionary<string, CsTeam>(new Dictionary<string, CsTeam>
+		public static readonly ReadOnlyDictionary<string, CsTeam> TeamSkins = new(new Dictionary<string, CsTeam>
 		{
-			// Counter-Terrorists (CT)
-			{ "weapon_usp_silencer", CsTeam.CounterTerrorist },
+            // Counter-Terrorists (CT)
+            { "weapon_usp_silencer", CsTeam.CounterTerrorist },
 			{ "weapon_m4a1_silencer", CsTeam.CounterTerrorist },
 			{ "weapon_m4a1", CsTeam.CounterTerrorist },
 			{ "weapon_famas", CsTeam.CounterTerrorist },
@@ -33,8 +32,8 @@ namespace K4AlwaysWeaponSkins
 			{ "weapon_fiveseven", CsTeam.CounterTerrorist },
 			{ "weapon_scar20", CsTeam.CounterTerrorist },
 
-			// Terrorists (T)
-			{ "weapon_glock", CsTeam.Terrorist },
+            // Terrorists (T)
+            { "weapon_glock", CsTeam.Terrorist },
 			{ "weapon_tec9", CsTeam.Terrorist },
 			{ "weapon_ak47", CsTeam.Terrorist },
 			{ "weapon_galilar", CsTeam.Terrorist },
@@ -45,111 +44,64 @@ namespace K4AlwaysWeaponSkins
 
 		public override void Load(bool hotReload)
 		{
-			VirtualFunctions.GiveNamedItemFunc.Hook(OnGiveNamedItemPost, HookMode.Pre);
-
-			RegisterEventHandler((EventPlayerTeam @event, GameEventInfo info) =>
-			{
-				var player = @event.Userid;
-				if (player is null || !player.IsValid || player.IsBot || player.IsHLTV || !HandledPlayers.ContainsKey(player))
-					return HookResult.Continue;
-
-				info.DontBroadcast = true;
-				return HookResult.Changed;
-			}, HookMode.Pre);
+			GetTeamNumber.Hook(OverrideHook, HookMode.Pre);
+			VirtualFunctions.GiveNamedItemFunc.Hook(OverrideGiveNamedItem, HookMode.Pre);
 		}
 
-		public override void Unload(bool hotReload)
+		private HookResult OverrideHook(DynamicHook h)
 		{
-			VirtualFunctions.GiveNamedItemFunc.Unhook(OnGiveNamedItemPost, HookMode.Pre);
-		}
-
-		public HookResult OnGiveNamedItemPost(DynamicHook hook)
-		{
-			var weaponClass = hook.GetParam<string>(1);
-			if (string.IsNullOrEmpty(weaponClass) || !weaponClass.StartsWith("weapon_"))
-				return HookResult.Continue;
-
-			if (!TeamSkins.TryGetValue(weaponClass, out CsTeam requiredTeam))
-				return HookResult.Continue;
-
-			var itemServices = hook.GetParam<CCSPlayer_ItemServices>(0);
+			var itemServices = h.GetParam<CCSPlayer_ItemServices>(0);
 			var player = GetPlayerFromItemServices(itemServices);
-			if (player == null)
-				return HookResult.Continue;
 
-			if (HandledPlayers.ContainsKey(player))
+			if (player != null && player.IsValid && PlayerSkins.TryGetValue(player, out Queue<string>? queue) && queue.TryDequeue(out string? weapon) && TeamSkins.TryGetValue(weapon, out CsTeam team))
 			{
-				if (requiredTeam != player.Team) // ? This section handles that, when multiple items given at once and the teams are mixed, its gonna process them separately
-				{
-					if (!TeamWeapons.ContainsKey(player))
-						TeamWeapons[player] = [];
-
-					if (!TeamWeapons[player].Contains(weaponClass))
-						TeamWeapons[player].Add(weaponClass);
-					return HookResult.Handled;
-				}
-
-				return HookResult.Continue;
-			}
-
-			if (player.Team != requiredTeam)
-			{
-				HandleTeamSwitch(player, requiredTeam);
+				h.SetReturn((int)team);
+				return HookResult.Handled;
 			}
 
 			return HookResult.Continue;
 		}
 
-		private void HandleTeamSwitch(CCSPlayerController player, CsTeam requiredTeam)
+		private HookResult OverrideGiveNamedItem(DynamicHook h)
 		{
-			var originalTeam = player.Team;
-			HandledPlayers[player] = originalTeam;
-			player.SwitchTeam(requiredTeam);
+			string weapon = h.GetParam<string>(1);
+			if (string.IsNullOrEmpty(weapon) || !weapon.Contains("weapon"))
+				return HookResult.Continue;
 
-			Server.RunOnTick(32, () => // ? We give some time for other actions aswell
+			var itemServices = h.GetParam<CCSPlayer_ItemServices>(0);
+			var player = GetPlayerFromItemServices(itemServices);
+			if (player == null || !player.IsValid || player.IsBot)
+				return HookResult.Continue;
+
+			if (!PlayerSkins.TryGetValue(player, out Queue<string>? queue))
 			{
-				if (IsValidPlayer(player))
-				{
-					player.SwitchTeam(originalTeam);
-					HandledPlayers.Remove(player);
+				queue = new Queue<string>();
+				PlayerSkins[player] = queue;
+			}
 
-					Server.NextWorldUpdate(() =>
-					{
-						if (TeamWeapons.TryGetValue(player, out List<string>? value))
-						{
-							foreach (var weapon in value)
-							{
-								player.GiveNamedItem(weapon);
-							}
+			queue.Enqueue(weapon);
 
-							TeamWeapons.Remove(player);
-						}
-					});
-				}
-				else
-					HandledPlayers.Remove(player);
-			});
+			return HookResult.Continue;
+		}
+
+		public override void Unload(bool hotReload)
+		{
+			GetTeamNumber.Unhook(OverrideHook, HookMode.Pre);
+			VirtualFunctions.GiveNamedItemFunc.Unhook(OverrideGiveNamedItem, HookMode.Pre);
 		}
 
 		public static CCSPlayerController? GetPlayerFromItemServices(CCSPlayer_ItemServices itemServices)
 		{
-			if (itemServices.Pawn?.Value is not CBasePlayerPawn pawn || !pawn.IsValid)
-				return null;
+			if (itemServices?.Pawn?.Value is CBasePlayerPawn pawn && pawn.IsValid && pawn.Controller?.IsValid == true && pawn.Controller.Value != null)
+			{
+				var player = new CCSPlayerController(pawn.Controller.Value.Handle);
+				if (player.IsValid && !player.IsBot && !player.IsHLTV && player.Connected == PlayerConnectedState.PlayerConnected)
+				{
+					return player;
+				}
+			}
 
-			if (pawn.Controller?.Value == null || !pawn.Controller.IsValid)
-				return null;
-
-			var player = new CCSPlayerController(pawn.Controller.Value.Handle);
-
-			return IsValidPlayer(player) ? player : null;
-		}
-
-		private static bool IsValidPlayer(CCSPlayerController player)
-		{
-			return player.IsValid
-				&& !player.IsBot
-				&& !player.IsHLTV
-				&& player.Connected == PlayerConnectedState.PlayerConnected;
+			return null;
 		}
 	}
 }
